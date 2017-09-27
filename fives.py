9d83c91492f95ecf8d2acffe1b35cceaf391b241
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-27 13:56:12 -0400

    Updated from Brython Server: 9/27/2017 1:56:12 PM

--- fives.py
+++ fives.py
@@ -1,1 +1,11 @@
+#Charlie Goodrich
+#09/27/17
+#fives.py - prints out the number of multiples of 5 in a number
 
+number = int(input("Enter a number: "))
+
+amountLeft = number//5
+i = 1
+while i <= amountLeft:
+    print(i*5)
+    i += 1
