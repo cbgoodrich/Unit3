623915f475949922c03be9d34fbfc38f82191cbe
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-27 13:28:43 -0400

    Updated from Brython Server: 9/27/2017 1:28:43 PM

--- loopDemo.py
+++ loopDemo.py
@@ -4,5 +4,10 @@
 
 #print I love computer science 5 times
 
+"""
 for i in range(0, 5):
-    print("I <3 computer science")
+    print("I <3 computer science")"""
+    
+# print the numbers from 1 to 20
+for i in range(1,21):
+    print(i)
