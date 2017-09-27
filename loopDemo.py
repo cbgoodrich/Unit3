501b97d4b7a87a5089bb621697e25d04622efe46
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-27 13:37:00 -0400

    Updated from Brython Server: 9/27/2017 1:37:00 PM

--- loopDemo.py
+++ loopDemo.py
@@ -7,11 +7,22 @@
 for i in range(0, 5):
     print("I <3 computer science")"""
 
+i = 1
+
+
 """    
 #print the numbers from 1 to 20
 for i in range(1,21):
     print(i)"""
-    
+
+"""    
 #print out the odd numbers from 13 to 27
-for i in range(13, 28, 2):
-    print(i)
+for i in range(13, 28, 2): #the third number tells you how many to do
+    print(i)"""
+
+"""
+#add up the numbers from 1 to 5
+total = 0
+for i in range(1, 6):
+    total = total + i
+print(total)"""
