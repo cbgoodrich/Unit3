3f4fdacb0e71fdf3fa5b2c0ae8d6de6865041933
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-27 17:01:56 -0400

    Updated from Brython Server: 9/27/2017 5:01:55 PM

--- gauss.py
+++ gauss.py
@@ -1,1 +1,13 @@
+#Charlie Goodrich
+#09/27/17
+#gauss.py - prints the sum between two numbers
 
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+difference = int(input("Enter the desired gap between the numbers: "))
+
+total = 0
+for i in range(num1, num2+1, difference):
+    total += i
+    i += 1
+print(total)
