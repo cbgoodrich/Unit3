9f28450462cf22cab15f3a621486ab09963c5f58
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-27 13:32:25 -0400

    Updated from Brython Server: 9/27/2017 1:32:25 PM

--- loopDemo.py
+++ loopDemo.py
@@ -2,12 +2,16 @@
 #09/27/17
 #loopDemp.py - demo for loops
 
-#print I love computer science 5 times
-
 """
+#print I love computer science 5 times
 for i in range(0, 5):
     print("I <3 computer science")"""
-    
-# print the numbers from 1 to 20
+
+"""    
+#print the numbers from 1 to 20
 for i in range(1,21):
+    print(i)"""
+    
+#print out the odd numbers from 13 to 27
+for i in range(13, 28, 2):
     print(i)
