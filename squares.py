f161f9de2c95e719ed81fe1f21a3409c089f2fae
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-27 13:59:35 -0400

    Updated from Brython Server: 9/27/2017 1:59:35 PM

--- squares.py
+++ squares.py
@@ -1,1 +1,10 @@
+#Charlie Goodrich
+#09/27/17
+#squares.py - prints out all the squares up to a number
 
+number = int(input("Enter a number: "))
+
+i = 1
+while i <= number:
+    print(i**2)
+    i += 1
