20b6038437e78efa401d41c703a6f2f3c53293f5
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-28 14:38:58 -0400

    Updated from Brython Server: 9/28/2017 2:38:57 PM

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,1 +1,18 @@
+#Charlie Goodrich
+#09/28/17
+#numberGuessingGame.py - makes you guess a random number, tells you the number of tries
 
+from random import randint
+
+number = randint(1,100)
+guessAttempts = 1
+while True:
+    guess = int(input("Guess a number between 1 and 100: "))
+    if guess < number:
+        print("Guess higher")
+    elif guess > number:
+        print("Guess lower")
+    else:
+        break
+    print("It only took you", guessAttempts, "tries to get it")
+    guessAttempts += 1
