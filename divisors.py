c60043a5dd630763912498042b83bb78120cf293
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-28 14:48:43 -0400

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,1 +1,9 @@
+#Charlie Goodrich
+#09/28/17
+#divisors.py - asks the user to input a number, prints all of its divisors
 
+number = int(input("Enter a number: "))
+i = 1
+while number%i == 0:
+    print(number / i)
+    i += 1
