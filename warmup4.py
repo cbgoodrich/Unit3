5be2b7a08bcd19e6811e06816331e58aef232195
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-28 14:20:46 -0400

    Update warmup4.py

--- warmup4.py
+++ warmup4.py
@@ -1,1 +1,11 @@
+#Charlie Goodrich
+#09/28/17
+#warmup4.py - should actually be called warmup6.py. I can't read
 
+i = 1
+while i <= 10:
+    print(i, "mississippi")
+    i += 1
+
+for i in range(1,11):
+    print(i, "mississippi")
