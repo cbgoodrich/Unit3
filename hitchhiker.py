15ee6b511d172f2dee033afbf9aea31ff14ef8b2
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-28 14:24:27 -0400

    Updated from Brython Server: 9/28/2017 2:24:27 PM

--- hitchhiker.py
+++ hitchhiker.py
@@ -1,1 +1,9 @@
+#Charlie Goodrich
+#09/28/17
+#hitchhiker.py - prints 42 unless you say quit
 
+while True:
+    text = input("Enter a question or type 'quit': ")
+    if text == "quit":
+        break
+    print(42)
