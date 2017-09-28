a72d43a4689e39e96d3c0c8f54c5b5b7f4f158d2
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-09-28 14:28:33 -0400

    Updated from Brython Server: 9/28/2017 2:28:33 PM

--- unitConverter.py
+++ unitConverter.py
@@ -1,1 +1,34 @@
+#Charlie Goodrich
+#09/13/17
+#unitConverter.py - converts units
 
+while True:
+    print("1) Kilometers to miles")
+    print("2) Kilgrams to pounds")
+    print("3) Liters to gallons")
+    print("4) Celsius to Fahrenheit")
+
+    number = int(input("Choose a conversion or type 'quit': "))
+
+    if number == 1:
+        distKM = float(input("Enter distance in kilometers: "))
+        distMI = distKM * 0.621371
+        print(distKM, "kilometers is", distMI, "miles")
+
+    elif number == 2:
+        massKG = float(input("Enter a mass in kilograms: "))
+        massLBS = massKG * 2.20462
+        print(massKG, "kilograms is", massLBS, "pounds")
+    elif number == 3:
+        volumeL = float(input("Enter a volume in liters: "))
+        volumeGal = volumeL * 0.264172
+        print(volumeL, "liters is", volumeGal, "gallons")
+    elif number == 4:
+        tempC = float(input("Enter a temperature in Celsius: "))
+        tempF = tempC * 1.8 + 32
+        print(tempC, "degrees Celsius is", tempF, "degrees Fahrenheit")
+    if str(number) == "quit":
+        break
+    else:
+        print("Error. Try again. This time use a number between 1 and 4")
+    
