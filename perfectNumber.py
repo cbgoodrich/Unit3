f6ac69bf52c7e924ff1d69f3f687576913b779b4
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-02 14:35:55 -0400

    Updated from Brython Server: 10/2/2017 2:35:48 PM

--- perfectNumber.py
+++ perfectNumber.py
@@ -1,1 +1,18 @@
+#Charlie Goodrich
+#10/02/17
+#perfectNumber.py - tells you if you have a perfect number
 
+number = int(input("Enter a number: "))
+
+i = 1
+total = 0
+
+while i < number:
+    if number%i == 0:
+        div = number/i
+        total += i
+    i += 1
+if total == number:
+    print(number, "is perfect")
+else:
+    print(number, "is not perfect")
