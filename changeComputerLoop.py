d70c9647e1163b198d196f5428688bbcc139ebf0
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-02 14:55:47 -0400

    Updated from Brython Server: 10/2/2017 2:55:46 PM

--- changeComputerLoop.py
+++ changeComputerLoop.py
@@ -1,1 +1,31 @@
+#Charlie Goodrich
+#10/02/17
+#computerChangeLoop.py - tells you the change you have
 
+centTotal = int(input("How many cents do you have? "))
+
+
+quarters = 0
+while centTotal >= 25:
+    centTotal = centTotal - 25
+    quarters += 1
+print("Quarters:", quarters)
+
+dimes = 0
+while centTotal >= 10:
+    centTotal = centTotal - 10
+    dimes += 1
+print("Dimes:", dimes)
+
+nickels = 0
+while centTotal >= 5:
+    centTotal = centTotal - 5
+    nickels += 1
+print("Nickels:", nickels)
+
+pennies = 0
+while centTotal >= 1:
+    centTotal = centTotal - 1
+    pennies += 1
+print("Pennies:", pennies)
+    
