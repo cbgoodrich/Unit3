6d34ac4c1fe9611a6a33799b2752bd23ed3336c5
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-02 14:21:29 -0400

    Updated from Brython Server: 10/2/2017 2:21:28 PM

--- warmup8.py
+++ warmup8.py
@@ -1,1 +1,12 @@
+#Charlie Goodrich
+#10/02/17
+#warmup8.py - finds the sum of all positive integers less than 100,000 divisible by 3, 10, and 17
 
+i = 1
+total = 0
+while i < 100000:
+    if i%3 == 0 and i%10 == 0 and i%17 == 0:
+        print(total)
+    i += 1
+    total += i
+    
