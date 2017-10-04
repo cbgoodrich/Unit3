0efd81098cbfdd9751ddd9636f6b69f1acf29507
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-04 13:57:58 -0400

    Updated from Brython Server: 10/4/2017 1:57:56 PM

--- fri13.py
+++ fri13.py
@@ -1,1 +1,16 @@
+#Charlie Goodrich
+#10/04/17
+#fri13.py - gives you the next 10 friday the 13ths
 
+from calendar import weekday
+
+fri13Counter = 0
+i = 0
+k = 0
+year = date.today().year
+month = date.today().month
+day = date.today().month
+
+while fri13Counter < 10:
+    
+    
