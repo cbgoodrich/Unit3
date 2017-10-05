0e84992a4f5aeeae951352ff7abf9fbdb85abe5c
cbgoodrich <31485791+cbgoodrich@users.noreply.github.com>
2017-10-05 14:42:20 -0400

    Updated from Brython Server: 10/5/2017 2:42:20 PM

--- warmup10.py
+++ warmup10.py
@@ -1,1 +1,21 @@
+#Charlie Goodrich
+#10/04/17
+#warmup10.py - making wallpaper
 
+from ggame import *
+
+black = Color(0x000000, 1)
+red = Color(0xFF0000, 1)
+yellow = Color(0xFFFF00, 1)
+
+blackRect = RectangleAsset(200, 100, LineStyle(1, black), black)
+redRect = RectangleAsset(200, 67, LineStyle(1, red), red)
+yellowRect = RectangleAsset(200, 33, LineStyle(1, yellow), yellow)
+
+for j in range(10):
+    for i in range(10):
+        Sprite(blackRect, (0 + 250*i, 0 + 150*j))
+        Sprite(redRect, (0 + 250*i, 33 + 150*j))
+        Sprite(yellowRect, (0 + 250*i, 67 + 150*j))
+        
+App().run()
